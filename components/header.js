32baa750145d30bbd374e9b4a91e33355b436837
nitishkumar03382 <nitishkumar03382@gmail.com>
2022-05-09 23:59:55 +0530

    Day 1 What is ML

---
+++ components/header.js
@@ -0,0 +1,47 @@
+class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+      <style>
+        nav {
+          height: 40px;
+          display: flex;
+          align-items: center;
+         
+          background-color:  #0a0a23;
+        }
+        .link{
+            display: inline;
+        }
+       
+        
+        a {
+          font-weight: 700;
+          margin: 0 25px;
+          color: #fff;
+          text-decoration: none;
+        }
+        
+        a:hover {
+          padding-bottom: 5px;
+          box-shadow: inset 0 -2px 0 0 #fff;
+        }
+      </style>
+      <header>
+        <nav>
+          <ul>
+            <li class = 'link'><a href="./ml/day1intro.html">Machine Learning</a></li>
+            <li class = 'link'><a href="Web Dev.html">Web Dev</a></li>
+            <li class = 'link' style="float:right"><a href="about.html">About</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
+  }
+    
+}
+
+customElements.define('header-component', Header);
